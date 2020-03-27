b0dfc313fce1150a42d6a6c58602bee1e6c5f441
Symon <ramossy@oregonstate.edu>
2020-03-26 22:14:52 -0700

    add words router for the thesaurus api (had to implement this in the backend to handle cors policy instead of frontend)

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var poemsRouter = require('./routes/poems');
 var quotesRouter = require('./routes/quotes');
+var wordsRouter = require('./routes/words');
 
 InitiateMongoServer();
 
@@ -45,6 +46,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/poems', poemsRouter);
 app.use('/quotes', quotesRouter);
+app.use('/words', wordsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/words.js
+++ routes/words.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var thesaurus = require("powerthesaurus-api");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  // Callbacks:
+thesaurus('car', function(err, res) {
+  if (err) throw err
+  console.log(res)
+})
 
   res.render('index', { title: 'Express' });
 });
