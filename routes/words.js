40a8d2d6577472cfd8fbf79a2926fb169acf794d
Symon <ramossy@oregonstate.edu>
2020-03-26 22:19:34 -0700

    implement words thesaurus api

--- routes/words.js
+++ routes/words.js
@@ -1,18 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 var thesaurus = require("powerthesaurus-api");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
+  //Retrieve parameters from body (assumes application/json)
+  const { word, kind } = req.body;
 
+  /*
   // Callbacks:
-thesaurus('car', function(err, res) {
-  if (err) throw err
-  console.log(res)
-})
+  thesaurus("car", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+  */
 
-  res.render('index', { title: 'Express' });
+  // Promises and given a kind:
+  thesaurus(word, kind).then(
+    result => {
+      console.log(result);
+      res.status(200).json({
+        result
+      });
+    },
+    error => {
+      throw error;
+    }
+  );
+
 });
 
 module.exports = router;
