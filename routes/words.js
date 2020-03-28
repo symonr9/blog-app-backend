814f103f95eb3f1eab6f11e9405d1b55e3b4dc51
Symon <ramossy@oregonstate.edu>
2020-03-27 21:04:37 -0700

    clean up

--- routes/words.js
+++ routes/words.js
@@ -8,11 +8,6 @@ router.post("/", function(req, res, next) {
   //Retrieve parameters from body (assumes application/json)
   const { word, kind } = req.body;
 
-  
-  console.log("in words router");
-  console.log("word and kind are: ", word, " ", kind);
-
-  // Promises and given a kind:
   thesaurus(word, kind).then(
     data => {
       console.log(data);
