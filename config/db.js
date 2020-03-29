7eaad2aa037514a3d58a04d6a33b2283e6081746
Symon <ramossy@oregonstate.edu>
2020-03-28 18:18:49 -0700

    add db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-import MONGOURI from "./config.js";
 
 const InitiateMongoServer = async () => {
   try {
     await mongoose.connect(MONGOURI, {
+      useUnifiedTopology: true,
       useNewUrlParser: true
     });
     console.log("Connected to DB !!");
